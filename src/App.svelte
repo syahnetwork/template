3920f74e4b77dc6b7d42e46e51d5e836edd957eb
Adam <syahnetwork@gmail.com>
2020-04-07 08:15:01 +0700

    styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,44 @@
 <script>
   // export let name;
   // let name='adam'
-  let src=''
-  let name='rickrolled'
+  let src = "";
+  let name = "rickrolled";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-  <img {src} alt="{name} :)">
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+
+    p {
+      color: blue;
+      font-family: "Times New Roman", Times, cursive;
+      font-size: 5em;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <img {src} alt="{name} :)" />
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
