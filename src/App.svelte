babd1d858c9df79e01eccd1ccc3542ec46441a87
Adam <syahnetwork@gmail.com>
2020-04-07 09:20:11 +0700

    branch

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,15 @@
 <script>
-  // export let name;
-  // let name='adam'
-  let src = "";
-  let name = "rickrolled";
-  import NestedObject from './TestNested.svelte'
-  let htmlString = 'hello <strong>Adam</strong>;'
+  let count = 0;
+
+  function countClick() {
+    count += 1;
+  }
 </script>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-
-    p {
-      color: blue;
-      font-family: "Times New Roman", Times, cursive;
-      font-size: 5em;
-    }
-  }
 </style>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <img {src} alt="{name} :)" />
-  <p>
-    calling components from ... 
-  </p><NestedObject/>
-  <p>{@html htmlString}</p>
-</main>
+<button on:click={countClick}>
+  clicked {count} {count === 1 || count === 0 ? 'time' : 'times'}
+</button>
