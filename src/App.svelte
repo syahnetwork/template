157e47f914593f7ea9fbc0b7198e359c1de4a945
Adam <syahnetwork@gmail.com>
2020-04-08 22:37:30 +0700

    branch19

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,5 @@
 <script>
   let m = { x: 0, y: 0 };
-
-  function handleMousemove(event) {
-    m.x = event.clientX;
-    m.y = event.clientY;
-  }
 </script>
 
 <style>
@@ -14,5 +9,6 @@
   }
 </style>
 
-move your mouse :)
-<div on:mousemove={handleMousemove}>mouse position is {m.x} x {m.y}</div>
+<div on:mousemove="{e => m = { x: e.clientX, y: e.clientY }}">
+  the mouse position is {m.x} x {m.y}
+</div>
