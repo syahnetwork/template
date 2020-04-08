1f82f0061a86608c278ebfc816959b2d6466342d
Adam <syahnetwork@gmail.com>
2020-04-08 23:04:35 +0700

    25

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,18 @@
 <script>
-  let name = "";
+  let a = 1;
+  let b = 2;
 </script>
 
-<input type=" text" bind:value={name} placeholder="enter name" />
-<p>hello {name || 'stranger'}!</p>
+<label for="">
+  <input type="number" bind:value={a} min="0" max="10" />
+  <input type="range" bind:value={a} min="0" max="10" />
+
+</label>
+
+<label for="">
+  <input type="number" bind:value={b} min="0" max="10" />
+  <input type="range" bind:value={b} min="0" max="10" />
+
+</label>
+<!-- wow binding realtime changeable data -->
+<p>{a}+{b}={a + b}</p>
