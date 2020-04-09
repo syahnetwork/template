97f3bd939abb63fbdb80208631b769f6eddf5d63
Adam <syahnetwork@gmail.com>
2020-04-09 12:39:14 +0700

    33 good UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,14 @@
 <script>
-  let w;
-  let h;
-  let size = 42;
-  let text = "edit me";
+  import Keypad from "./Keypad.svelte";
+
+  let pin;
+  $: view = pin ? pin.replace(/\d(?!$)/g, "•") : "enter your pin";
+
+  function handleSubmit() {
+    alert(`submitted ${pin}`);
+  }
 </script>
 
-<style>
-  input {
-    display: block;
-  }
-  div {
-    display: inline-block;
-  }
-</style>
+<h1 style="color:{pin ? 'orange' : 'red'}">{view}</h1>
 
-<input type="range" bind:value={size} />
-<input type="text" bind:value={text} />
-
-<p>size : {w}px x{h}px</p>
-
-<div bind:clientWidth={w} bind:clientHeight={h}>
-  <span style="font-size: {size}px">{text}</span>
-</div>
+<Keypad bind:value={pin} on:submit={handleSubmit} />
