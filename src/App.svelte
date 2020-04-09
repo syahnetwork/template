d88328479faf2df779ea05778c6f0b2084a911bd
Adam <syahnetwork@gmail.com>
2020-04-09 08:44:15 +0700

    28

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,43 @@
 <script>
-// for some reasons marked not properly imported
-	import marked from 'marked';
-	let text = `Some words are *italic*, some are **bold**`;
+  let questions = [
+    { id: 1, text: `where go to school?` },
+    { id: 2, text: `what is your mother's name?` },
+    { id: 3, text: `what is googling?` }
+  ];
+
+  let selected;
+  let answer = "";
+
+  function handleSubmit() {
+    alert(
+      `answered question ${selected.id} (${selected.text}) with "${answer}"`
+    );
+  }
 </script>
 
 <style>
-	textarea { width: 100%; height: 200px; }
+  input {
+    display: block;
+    width: 500px;
+    max-width: 100%;
+  }
 </style>
 
-<textarea bind:value={text}></textarea>
+<h2>insecurity questions</h2>
 
-{@html marked(text)}
+<form action="" on:submit|preventDefault={handleSubmit}>
+  <select name="" id="" bind:value={selected} on:change={() => (answer = '')}>
+
+    {#each questions as question}
+      <option value={question}>{question.text}</option>
+    {/each}
+
+  </select>
+
+  <input type="text" bind:value={answer} />
+
+  <button disabled={!answer} type="submit">submit</button>
+
+  <p>selected question {selected ? selected.id : '[waiting...]'}</p>
+
+</form>
