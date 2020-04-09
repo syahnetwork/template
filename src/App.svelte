4ae9037a98ffe877c2eba0f916888afd594a1afc
Adam <syahnetwork@gmail.com>
2020-04-09 08:34:17 +0700

    28

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,13 @@
 <script>
-  let scoops = 1;
-  let flavours = ["mint choc chip"];
-
-  let menu = ["cookies and cream", "mint choc chip", "raspberry ripple"];
-
-  function join(flavours) {
-    if (flavours.length === 1) return flavours[0];
-    return `${flavours.slice(0, -1).join(",")} and ${
-      flavours[flavours.length - 1]
-    }`;
-  }
+// for some reasons marked not properly imported
+	import marked from 'marked';
+	let text = `Some words are *italic*, some are **bold**`;
 </script>
 
-<h2>size</h2>
+<style>
+	textarea { width: 100%; height: 200px; }
+</style>
 
-<label for="">
-  <input type="radio" bind:group={scoops} value={1} />
-  one scoop
-</label>
+<textarea bind:value={text}></textarea>
 
-<label for="">
-  <input type="radio" bind:group={scoops} value={2} />
-  two scoop
-</label>
-
-<label for="">
-  <input type="radio" bind:group={scoops} value={3} />
-  three scoop
-</label>
-
-<h2>flavours</h2>
-
-{#each menu as flavours}
-  <label for="">
-    <input type="checkbox" bind:group={flavours} value="flavour" />
-    {flavour}
-  </label>
-{/each}
-
-{#if flavours.length === 0}
-  <p>please select one flavour</p>
-{:else if flavours.length > scoops}
-  <p>can't order more flavours than scoops</p>
-{:else}
-  <p>
-    you ordered {scoops} {scoops === 1 ? 'scoop' : 'scoops'} of {join(flavours)}
-  </p>
-{/if}
+{@html marked(text)}
