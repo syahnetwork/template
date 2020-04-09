1e2e12c0f6ea71b7958d2fd62576014ad0083433
Adam <syahnetwork@gmail.com>
2020-04-09 09:15:32 +0700

    2

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
-let src='tutorial/image.gif'
+  let src = "tutorial/image.gif";
+  let name = "rick astley";
 </script>
 
 <!-- <img src={src} alt=""> -->
-<img {src} alt="dancing">
+<img {src} alt="{name} dancing" />
